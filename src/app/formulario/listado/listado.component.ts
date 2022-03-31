df2237bd49a59f9a3fb712ff00a4f13b1811c642
Sebastian Saldana <jsaldana.idrobo@gmail.com>
2022-03-31 16:19:13 -0500

    Funcion para enviar el post con el dato de prueba a la URL

--- src/app/formulario/listado/listado.component.ts
+++ src/app/formulario/listado/listado.component.ts
@@ -1,139 +1,142 @@
 import { Component } from '@angular/core';
-import directorio from '../../files/directorio.json'
+import directorio from '../../files/directorio.json';
 
 interface BotonDireccion {
-  nombre      : string;
-  abreviatura : string;
-  tipo        : string;
+  nombre: string;
+  abreviatura: string;
+  tipo: string;
 }
 
 interface Validaciones {
-  tipo : string;
-  funcion : () => void;
+  tipo: string;
+  funcion: () => void;
 }
 
-@Component({ 
+@Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: ['./listado.component.css']
+  styleUrls: ['./listado.component.css'],
 })
+export class ListadoComponent {
+  varDireccion: string[] = [];
+  varTipoDireccion: string[] = [];
+  varDirCofificada: string[] = [];
+  ultimaPosicion: number = this.varTipoDireccion.length - 1;
+  ultimoTipo: string = this.varTipoDireccion[this.ultimaPosicion];
+  penUltimoTipo: string = this.varDireccion[this.ultimaPosicion - 1];
+  ultimaDireccion: string = this.varDireccion[this.ultimaPosicion];
+  directorioList: BotonDireccion[] = directorio;
 
-export class ListadoComponent{
+  datosSelector: BotonDireccion[] = this.directorioList.filter((dir) => {
+    return dir.tipo === 'opcion' || dir.tipo === 'nomenclatura';
+  });
 
-  varDireccion      : string[] = [];
-  varTipoDireccion  : string[] = [];
-  varDirCofificada  : string[] = [];
-  ultimaPosicion    : number   = this.varTipoDireccion.length - 1; 
-  ultimoTipo        : string   = this.varTipoDireccion[this.ultimaPosicion];
-  penUltimoTipo     : string   = this.varDireccion[this.ultimaPosicion - 1];
-  ultimaDireccion   : string   = this.varDireccion[this.ultimaPosicion];
-  directorioList    : BotonDireccion[] = directorio;
+  datosNomenclatura: BotonDireccion[] = this.directorioList.filter((dir) => {
+    return dir.tipo === 'nomenclatura';
+  });
 
-  datosSelector     : BotonDireccion[] = this.directorioList.filter((dir) => {
-                        return dir.tipo === 'opcion' || dir.tipo === 'nomenclatura';
-                      });
+  datosNumeros: BotonDireccion[] = this.directorioList.filter((dir) => {
+    return dir.tipo === 'numero' || dir.tipo === 'especial';
+  });
 
-  datosNomenclatura : BotonDireccion[] = this.directorioList.filter((dir) => {
-                      return dir.tipo === 'nomenclatura';
-                      });
+  datosLetras: BotonDireccion[] = this.directorioList.filter((dir) => {
+    return dir.tipo === 'letra';
+  });
 
-  datosNumeros      : BotonDireccion[] = this.directorioList.filter((dir) => {
-                        return dir.tipo === 'numero' || dir.tipo === 'especial';
-                      });
-
-  datosLetras       : BotonDireccion[] = this.directorioList.filter((dir) => {
-                        return dir.tipo === 'letra';
-                      });
-
-  constructor() { 
-
-  }
-  // agregar() { 
+  constructor() {}
+  // agregar() {
   //   console.log('Esto es una prueba');
   // }
-  
-  validarDirecciones( boton : BotonDireccion ):void {
-    
-    let tipo      : string = boton.tipo;
-    let tipoTipos : string[] = ['opcion','nomenclatura','numero','especial','letra'];
 
-    if (this.varDireccion[0] === undefined ) {
+  validarDirecciones(boton: BotonDireccion): void {
+    let tipo: string = boton.tipo;
+    let tipoTipos: string[] = [
+      'opcion',
+      'nomenclatura',
+      'numero',
+      'especial',
+      'letra',
+    ];
+
+    if (this.varDireccion[0] === undefined) {
       if (tipo === ('nomenclatura' || 'opcion')) {
-        this.llenarDirecciones( boton );
+        this.llenarDirecciones(boton);
       } else {
         console.log('Alert ->Debe ser tipo nomenclatura');
-      }      
+      }
     } else {
-      tipo === ('nomenclatura' || 'opcion') ? this.nomenclaturaIgual( boton ) : false
-
+      tipo === ('nomenclatura' || 'opcion')
+        ? this.nomenclaturaIgual(boton)
+        : false;
     }
-
   }
 
-  nomenclaturaIgual( boton : BotonDireccion ): void {
-
+  nomenclaturaIgual(boton: BotonDireccion): void {
     this.ultimoTipo !== ('nomenclatura' || 'opcion')
-                    ? this.llenarDirecciones( boton ) 
-                    : console.log('Alert -> No se puede ingresar 2 nomenclaturas del mismo tipo');
-    
+      ? this.llenarDirecciones(boton)
+      : console.log(
+          'Alert -> No se puede ingresar 2 nomenclaturas del mismo tipo'
+        );
+
     console.log('Se esta ejecutando');
-
   }
 
-  letraIgual( boton : BotonDireccion ): void {
-
+  letraIgual(boton: BotonDireccion): void {
     this.ultimaDireccion !== boton.nombre
-                    ? this.llenarDirecciones( boton ) 
-                    : console.log('Alert -> No se puede ingresar la misma letra');
+      ? this.llenarDirecciones(boton)
+      : console.log('Alert -> No se puede ingresar la misma letra');
   }
-  
-  letrasSeguidas( boton : BotonDireccion ): void {
-    
+
+  letrasSeguidas(boton: BotonDireccion): void {
     (this.ultimoTipo && this.penUltimoTipo) === 'numero'
-    ? console.log('Alert -> No se puede ingresar 3 letras seguidas')
-    : this.llenarDirecciones( boton );
+      ? console.log('Alert -> No se puede ingresar 3 letras seguidas')
+      : this.llenarDirecciones(boton);
   }
-  
-  simbolosRepetidos( boton : BotonDireccion ): void {
 
+  simbolosRepetidos(boton: BotonDireccion): void {
     this.ultimaDireccion !== boton.nombre
-                    ? this.llenarDirecciones( boton ) 
-                    : console.log('Alert -> No se puede ingresar la misma letra');
+      ? this.llenarDirecciones(boton)
+      : console.log('Alert -> No se puede ingresar la misma letra');
   }
 
-  imprimirDirecciones( direccion: string[] ) : string {
-
+  imprimirDirecciones(direccion: string[]): string {
     let impDireccion = '';
-    direccion.forEach(element => {
-        impDireccion  += `${element} `;
+    direccion.forEach((element) => {
+      impDireccion += `${element} `;
     });
     return impDireccion;
-
   }
 
-  llenarDirecciones( boton : BotonDireccion ):void {
-    
-    this.varDireccion.push(boton.nombre); 
+  llenarDirecciones(boton: BotonDireccion): void {
+    this.varDireccion.push(boton.nombre);
     this.varDirCofificada.push(boton.abreviatura);
     this.varTipoDireccion.push(boton.tipo);
     console.log(this.varDireccion, this.varDirCofificada);
-
   }
 
   deshacer(): void {
-    this.varDireccion.pop(); 
+    this.varDireccion.pop();
     this.varDirCofificada.pop();
     console.log(this.varDireccion, this.varDirCofificada);
   }
 
-  limpiar():void {
-    this.varDireccion     = []; 
-    this.varDirCofificada = []; 
+  limpiar(): void {
+    this.varDireccion = [];
+    this.varDirCofificada = [];
     console.log(this.varDireccion, this.varDirCofificada);
   }
 
-  enviar():string {
-    return this.imprimirDirecciones(this.varDirCofificada);
-  }
+  enviar(): void {
+    const form = document.createElement('form');
+    form.method = 'post';
+    form.action = 'http://example.com/'; //Aquí va la URL del proyecto al que le va a llegar el dato
 
+    const hiddenField = document.createElement('input');
+    hiddenField.type = 'hidden';
+    hiddenField.name = 'test';
+    hiddenField.value = 'testeando la aplicacion';
+
+    document.body.appendChild(form);
+    form.submit();
+  }
 }

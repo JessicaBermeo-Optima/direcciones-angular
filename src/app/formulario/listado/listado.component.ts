400f1dcb0143e79ac0614ebb9b6b3564362512bc
JessicaBermeo-Optima <jessica.bermeo@optima.net.co>
2022-04-01 10:42:37 -0500

    Funcionalidad

--- src/app/formulario/listado/listado.component.ts
+++ src/app/formulario/listado/listado.component.ts
@@ -6,55 +6,25 @@ interface BotonDireccion {
   abreviatura: string;
   tipo: string;
 }
-<<<<<<< HEAD
 @Component({ 
-=======
-
-interface Validaciones {
-  tipo: string;
-  funcion: () => void;
-}
-
-@Component({
->>>>>>> df2237bd49a59f9a3fb712ff00a4f13b1811c642
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css'],
 })
 export class ListadoComponent {
-  varDireccion: string[] = [];
-  varTipoDireccion: string[] = [];
-  varDirCofificada: string[] = [];
-  ultimaPosicion: number = this.varTipoDireccion.length - 1;
-  ultimoTipo: string = this.varTipoDireccion[this.ultimaPosicion];
-  penUltimoTipo: string = this.varDireccion[this.ultimaPosicion - 1];
-  ultimaDireccion: string = this.varDireccion[this.ultimaPosicion];
-  directorioList: BotonDireccion[] = directorio;
-
-  datosSelector: BotonDireccion[] = this.directorioList.filter((dir) => {
-    return dir.tipo === 'opcion' || dir.tipo === 'nomenclatura';
-  });
-
-<<<<<<< HEAD
   varDireccion      : string[] = [];
   varTipoDireccion  : string[] = [];
   varDirCofificada  : string[] = [];
   directorioList    : BotonDireccion[] = directorio;
-=======
+
+  datosSelector     : BotonDireccion[] = this.directorioList.filter((dir) => {
+                      return dir.tipo === 'opcion' || dir.tipo === 'nomenclatura';
+                    });
+
   datosNomenclatura: BotonDireccion[] = this.directorioList.filter((dir) => {
-    return dir.tipo === 'nomenclatura';
-  });
->>>>>>> df2237bd49a59f9a3fb712ff00a4f13b1811c642
+                      return dir.tipo === 'nomenclatura';
+                    });
 
-  datosNumeros: BotonDireccion[] = this.directorioList.filter((dir) => {
-    return dir.tipo === 'numero' || dir.tipo === 'especial';
-  });
-
-  datosLetras: BotonDireccion[] = this.directorioList.filter((dir) => {
-    return dir.tipo === 'letra';
-  });
-
-<<<<<<< HEAD
   datosNumeros      : BotonDireccion[] = this.directorioList.filter((dir) => {
                         return dir.tipo === 'numero' || dir.tipo === 'especial';
                       });
@@ -125,62 +95,6 @@ export class ListadoComponent {
                                       :  ultimoNombre === nombre ? alert('No se puede ingresar 2 letras iguales')
                                       : this.llenarDirecciones( boton );
     }
-=======
-  constructor() {}
-  // agregar() {
-  //   console.log('Esto es una prueba');
-  // }
-
-  validarDirecciones(boton: BotonDireccion): void {
-    let tipo: string = boton.tipo;
-    let tipoTipos: string[] = [
-      'opcion',
-      'nomenclatura',
-      'numero',
-      'especial',
-      'letra',
-    ];
-
-    if (this.varDireccion[0] === undefined) {
-      if (tipo === ('nomenclatura' || 'opcion')) {
-        this.llenarDirecciones(boton);
-      } else {
-        console.log('Alert ->Debe ser tipo nomenclatura');
-      }
-    } else {
-      tipo === ('nomenclatura' || 'opcion')
-        ? this.nomenclaturaIgual(boton)
-        : false;
-    }
-  }
-
-  nomenclaturaIgual(boton: BotonDireccion): void {
-    this.ultimoTipo !== ('nomenclatura' || 'opcion')
-      ? this.llenarDirecciones(boton)
-      : console.log(
-          'Alert -> No se puede ingresar 2 nomenclaturas del mismo tipo'
-        );
-
-    console.log('Se esta ejecutando');
-  }
-
-  letraIgual(boton: BotonDireccion): void {
-    this.ultimaDireccion !== boton.nombre
-      ? this.llenarDirecciones(boton)
-      : console.log('Alert -> No se puede ingresar la misma letra');
-  }
-
-  letrasSeguidas(boton: BotonDireccion): void {
-    (this.ultimoTipo && this.penUltimoTipo) === 'numero'
-      ? console.log('Alert -> No se puede ingresar 3 letras seguidas')
-      : this.llenarDirecciones(boton);
-  }
-
-  simbolosRepetidos(boton: BotonDireccion): void {
-    this.ultimaDireccion !== boton.nombre
-      ? this.llenarDirecciones(boton)
-      : console.log('Alert -> No se puede ingresar la misma letra');
->>>>>>> df2237bd49a59f9a3fb712ff00a4f13b1811c642
   }
 
   imprimirDirecciones(direccion: string[]): string {
@@ -195,11 +109,6 @@ export class ListadoComponent {
     this.varDireccion.push(boton.nombre);
     this.varDirCofificada.push(boton.abreviatura);
     this.varTipoDireccion.push(boton.tipo);
-<<<<<<< HEAD
-
-=======
-    console.log(this.varDireccion, this.varDirCofificada);
->>>>>>> df2237bd49a59f9a3fb712ff00a4f13b1811c642
   }
 
   deshacer(): void {
@@ -207,16 +116,9 @@ export class ListadoComponent {
     this.varDirCofificada.pop();
   }
 
-<<<<<<< HEAD
   limpiar():void {
     this.varDireccion     = []; 
     this.varDirCofificada = []; 
-=======
-  limpiar(): void {
-    this.varDireccion = [];
-    this.varDirCofificada = [];
-    console.log(this.varDireccion, this.varDirCofificada);
->>>>>>> df2237bd49a59f9a3fb712ff00a4f13b1811c642
   }
 
   enviar(): void {

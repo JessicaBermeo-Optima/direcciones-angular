3e8439554b2ccbcca4266cbd7baabec725f89a25
JessicaBermeo-Optima <jessica.bermeo@optima.net.co>
2022-04-08 16:50:41 -0500

    Entrega Final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormularioComponent } from './formulario/formulario.module';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormularioComponent, AppRoutingModule],
+  imports: [BrowserModule, FormularioComponent, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
